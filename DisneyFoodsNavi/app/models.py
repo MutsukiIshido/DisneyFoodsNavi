0c58493d15d98a042d761b77ef9963ae3fe32eff
mtk <user@MutsukiMacBook-Air.local>
2024-12-23 12:48:05 +0900

    データベースの作成

--- DisneyFoodsNavi/app/models.py
+++ DisneyFoodsNavi/app/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    first_name = None
+    last_name = None
+    date_joined = None
+    groups = None
+    user_permissions = None
+    username = None
+    
+    nickname = models.CharField(max_length=64)
+    email = models.EmailField(max_length=128, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    
+    USERNAME_FIELD = "nickname"
+    EMAIL_FIELD = "email"
+    REQUIRED_FIELDS = ["email"]
+    
+    
+    class Meta:
+        db_table = "users"
