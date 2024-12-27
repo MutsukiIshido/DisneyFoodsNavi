a094672662a259a5f080dd79a4f5e829821fcea5
mtk <user@MutsukiMacBook-Air.local>
2024-12-28 02:43:06 +0900

    models.pyにモデルを追加

--- DisneyFoodsNavi/app/models.py
+++ DisneyFoodsNavi/app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 class User(AbstractUser):
@@ -23,3 +24,73 @@ class User(AbstractUser):
     
     class Meta:
         db_table = "users"
+        
+
+class Foods(models.Model):
+    foods_name = models.CharField(max_length=64)
+    price = models.IntegerField()
+    foods_image_path = models.CharField(max_length=255)
+    total_reviews = models.IntegerField()
+    average_rating = models.DecimalField(max_digits=2, decimal_places=1)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        db_table = "foods"
+        
+    def  __str__(self):
+        return self.foods_name
+
+
+class Areas(models.Model):
+    area_name = models.CharField(max_length=64)
+    park = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        db_table = "areas"
+        
+
+class Stores(models.Model):
+    store_name = models.CharField(max_length=24)
+    latitude = models.DecimalField(
+        max_digits=9,
+        decimal_places=6,
+        validators=[MinValueValidator(-90.0), MaxValueValidator(90.0)],
+        help_text="Latitude: a value between -90.0 and 90.0"
+    )
+    longitude = models.DecimalField(
+        max_digits=9,
+        decimal_places=6,
+        validators=[MinValueValidator(-180.0), MaxValueValidator(180.0)],
+        help_text="Longitude: a value between -180.0 and 180.0"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    area = models.OneToOneField(Areas, on_delete=models.CASCADE,  related_name="store", default=1)
+    foods = models.ManyToManyField(Foods, related_name="foods")
+    
+    class Meta:
+        db_table = "stores"
+
+
+class FoodStores(models.Model):
+    foods = models.ForeignKey(Foods, on_delete=models.CASCADE)
+    stores = models.ForeignKey(Stores, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        db_table = "food_stores"
+    
+    
+class FoodCategories(models.Model):
+    foods = models.ForeignKey(Foods, on_delete=models.PROTECT)
+    kind = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        db_table = "food_categories"
+        
