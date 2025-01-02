2a41a39392891183af32385e72ac7983e086222c
mtk <user@MutsukiMacBook-Air.local>
2025-01-03 01:43:43 +0900

    モデルの変更、マイグレーション

--- DisneyFoodsNavi/app/models.py
+++ DisneyFoodsNavi/app/models.py
@@ -1,6 +1,30 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.core.validators import MinValueValidator, MaxValueValidator
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, nickname, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        if not nickname:
+            raise ValueError('The Nickname field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, nickname=nickname, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, nickname, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(email, nickname, password, **extra_fields)
+
 
 
 class User(AbstractUser):
@@ -11,7 +35,7 @@ class User(AbstractUser):
     user_permissions = None
     username = None
     
-    nickname = models.CharField(max_length=64)
+    nickname = models.CharField(max_length=64,unique=True) # Uniqueを追加
     email = models.EmailField(max_length=128, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -21,6 +45,8 @@ class User(AbstractUser):
     EMAIL_FIELD = "email"
     REQUIRED_FIELDS = ["email"]
     
+    objects = CustomUserManager()  # カスタムマネージャーを指定
+
     
     class Meta:
         db_table = "users"
